d3628b4bacb3e80fa01a023add142a80ee950294
Kanwal <kanwal@example.com>
2026-05-17 10:10:06 -0400

    added new test case

--- test_app.py
+++ test_app.py
@@ -1,7 +1,22 @@
 from app import app
+import json
 
 def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
     assert b"Hello, CI/CD!" in response.data
+
+def test_add():
+    client = app.test_client()
+    response = client.get("/add?a=7&b=6")
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert data["result"] == 13
+
+def test_add_invalid():
+    client = app.test_client()
+    response = client.get("/add?a=invalid&b=6")
+    assert response.status_code == 400
+    data = json.loads(response.data)
+    assert "error" in data
